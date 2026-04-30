9bb77c068203e05309296379736336c3f3a3ca1d
João Henrique <jhenrique.caval@hotmail.com>
2026-04-30 00:35:02 -0300

    feat: configurando swagger para personalização

--- src/main/java/com/clinica/clinica_api/config/SwaggerConfig.java
+++ src/main/java/com/clinica/clinica_api/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package com.clinica.clinica_api.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Clínica API - João Henrique")
+                        .version("1.0")
+                        .description("API REST para gerenciamento de agendamentos de consultas"));
+    }
 }
