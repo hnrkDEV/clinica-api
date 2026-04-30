0a16ae9e7dfc839b81fb9943eca3e12affd5d7b4
João Henrique <jhenrique.caval@hotmail.com>
2026-04-30 00:33:51 -0300

    feat: criação do responseDTO para o agendamento

--- src/main/java/com/clinica/clinica_api/dto/response/AgendamentoResponse.java
+++ src/main/java/com/clinica/clinica_api/dto/response/AgendamentoResponse.java
@@ -1,4 +1,15 @@
 package com.clinica.clinica_api.dto.response;
 
-public class AgendamentoResponse {
-}
+import com.clinica.clinica_api.enums.StatusAgendamento;
+import com.clinica.clinica_api.enums.TipoAtendimento;
+
+import java.time.LocalDateTime;
+
+public record AgendamentoResponse(
+        Long id,
+        String paciente,
+        String profissional,
+        LocalDateTime dataHora,
+        TipoAtendimento tipoAtendimento,
+        StatusAgendamento status
+) {}
