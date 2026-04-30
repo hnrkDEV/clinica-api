3379d9fc3e5fa579b7f62583acb6ae836df9bb89
João Henrique <jhenrique.caval@hotmail.com>
2026-04-30 01:26:27 -0300

    feat: auditando a criação do paciente

--- src/main/java/com/clinica/clinica_api/service/impl/PacienteServiceImpl.java
+++ src/main/java/com/clinica/clinica_api/service/impl/PacienteServiceImpl.java
@@ -1,6 +1,8 @@
 package com.clinica.clinica_api.service.impl;
 
+import com.clinica.clinica_api.adapter.AuditoriaAdapter;
 import com.clinica.clinica_api.entity.Paciente;
+import com.clinica.clinica_api.repository.AuditoriaRepository;
 import com.clinica.clinica_api.repository.PacienteRepository;
 import com.clinica.clinica_api.service.PacienteService;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +15,17 @@ import java.util.List;
 public class PacienteServiceImpl implements PacienteService {
 
     private final PacienteRepository pacienteRepository;
+    private final AuditoriaRepository auditoriaRepository;
 
     @Override
     public Paciente cadastrar(Paciente paciente) {
-        return pacienteRepository.save(paciente);
+        Paciente pacienteSalvo = pacienteRepository.save(paciente);
+
+        auditoriaRepository.save(
+                AuditoriaAdapter.pacienteCadastrado(pacienteSalvo)
+        );
+
+        return pacienteSalvo;
     }
 
     @Override
